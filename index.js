5d730f44444d8116b4f5f313f07b1393486b84d4
HenryYenHa <henryha@live.ca>
2023-10-23 17:01:33 -0600

    Before cramming it all together

--- index.js
+++ index.js
@@ -21,3 +21,10 @@ function toContactPage() {
 function toSkillsPage() {
   window.location.href = "/skills.html";
 }
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
