9a8ae74c53296714411c59e37b0695c8a6af2c72
HenryYenHa <henryha@live.ca>
2023-10-23 22:09:38 -0600

    Background generated, still need alteration

---
+++ new-version/indexTrue.js
@@ -0,0 +1,30 @@
+/* TODO LIST:
+  - All functions to head to separate pages.
+
+*/
+
+function toHomePage() {
+  window.location.href = "index.html";
+}
+function toAboutPage() {
+  window.location.href = "/about.html";
+}
+function toAssembleCVPage() {
+  window.location.href = "/assembleCV.html";
+}
+function toProjectsPage() {
+  window.location.href = "/projects.html";
+}
+function toContactPage() {
+  window.location.href = "/contact.html";
+}
+function toSkillsPage() {
+  window.location.href = "/skills.html";
+}
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
