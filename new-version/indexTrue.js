5b0640ea8267a1d8d5badce807fd022f769bd16d
HenryYenHa <henryha@live.ca>
2023-10-24 14:17:47 -0600

    Depreciated things and added animation filter

--- new-version/indexTrue.js
+++ new-version/indexTrue.js
@@ -1,30 +1,12 @@
 /* TODO LIST:
-  - All functions to head to separate pages.
+  - Generate the cards for projects
 
 */
 
-function toHomePage() {
-  window.location.href = "index.html";
-}
-function toAboutPage() {
-  window.location.href = "/about.html";
-}
-function toAssembleCVPage() {
-  window.location.href = "/assembleCV.html";
-}
-function toProjectsPage() {
-  window.location.href = "/projects.html";
-}
-function toContactPage() {
-  window.location.href = "/contact.html";
-}
-function toSkillsPage() {
-  window.location.href = "/skills.html";
-}
-
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+/*Project objects will have the following details:
+- Name of project
+- URL of image for project
+- Date of release of project
+- Description of project
+- List of skills/tools used in the project
+*/
